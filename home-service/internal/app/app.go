c0281816c6a8487890febc4cd0c238e277e0ee49
Miroslav <guresq@gmail.com>
2024-08-17 15:38:35 +0300

    fix folder

--- home-service/internal/app/app.go
+++ home-service/internal/app/app.go
@@ -4,7 +4,7 @@ import (
 	"log/slog"
 
 	"home-service/internal/adapters/db/postgres"
-	"home-service/internal/app/grpc"
+	"home-service/internal/app/grpc/home"
 	"home-service/internal/services/home"
 	"home-service/pkg/config"
 )
@@ -14,7 +14,7 @@ type App struct {
 }
 
 func New(log *slog.Logger, cfg *config.GRPCConfig, db *postgres.DbPostgres) *App {
-	homeService := home.New(log)
+	homeService := home.New(log, db, db, db, db, db, db, db)
 	grpcApp := grpc.New(log, homeService, cfg)
 
 	return &App{
