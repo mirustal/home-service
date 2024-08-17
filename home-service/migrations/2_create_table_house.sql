fc3abe231283f0d8ae1d357ad1d442c30241184c
Miroslav <guresq@gmail.com>
2024-08-17 15:39:03 +0300

    add migrate

---
+++ home-service/migrations/2_create_table_house.sql
@@ -0,0 +1,44 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE houses (
+    id SERIAL PRIMARY KEY,
+    address VARCHAR(255) NOT NULL,
+    year INT CHECK (year > 0),
+    developer VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE flats (
+    id SERIAL PRIMARY KEY,
+    house_id INT REFERENCES houses(id) ON DELETE CASCADE,
+    price INT CHECK (price >= 0),
+    rooms INT CHECK (rooms > 0),
+    status VARCHAR(20) CHECK (status IN ('created', 'approved', 'declined', 'on moderation')),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+
+CREATE TABLE subscriptions (
+    id SERIAL PRIMARY KEY,
+    house_id INT REFERENCES houses(id) ON DELETE CASCADE,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_flats_house_id ON flats (house_id);
+CREATE INDEX idx_flats_status ON flats (status);
+
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS refresh_tokens;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS apps;
+
+DROP INDEX IF EXISTS idx_flats_house_id;
+DROP INDEX IF EXISTS idx_flats_status;
+-- +goose StatementEnd
