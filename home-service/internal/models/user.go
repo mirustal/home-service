7d33175ad1e4a8c85729cc48a20ea9e5b7eedb8b
Miroslav <guresq@gmail.com>
2024-08-17 15:38:59 +0300

    fix model

--- home-service/internal/models/user.go
+++ home-service/internal/models/user.go
@@ -4,9 +4,5 @@ import "time"
 
 type User struct {
     ID        string `json:"id" db:"id"`
-    Email     string    `json:"email" db:"email"`
-    HashPass  []byte    `json:"password" db:"password"`
-    Type      string    `json:"user_type" db:"user_type"`
-    CreatedAt time.Time `json:"created_at" db:"created_at"`
-    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+    Token       string `json:"token"`
 }
